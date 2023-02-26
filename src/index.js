0beb5ef846b197ca7a59e44834a2f618e6021327
MoniqueOg <moniqueogunk@icloud.com>
2023-02-26 23:57:57 +0000

    pushed

--- src/index.js
+++ src/index.js
@@ -21,39 +21,7 @@ app.use(express.raw());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-/*const todosbuttn = document.createTextNode("Full todos.");
-  todosbuttn.setAttribute("class", "todoslnk");
-  todosbuttn.setAttribute("href", "/todos");
-  todos.appendChild(todosbuttn);*/
-
-
-/*import { routes } from '../Router';
-
-const todoBtns = [
-  {
-    route: routes.todos,
-    property: `${routes.todos}/todos`,
-    text: 'Full Todo List',
-  },
-  {
-    route: routes.overdue,
-    id: `${routes.due}/todos/overdue`,
-    text: 'Overdue',
-  },
-  {
-    route: routes.completed,
-    property: `${routes.completed}/todos/completed`,
-    text: 'Completed',
-  },
-];
-
-  <Fragment>
-  <div className={styles.todoBtns}>
-  {todoBtns.map((item) => (
-      <TodoBtn key={item.text} route={item.route} icon={item.icon} text={item.text} />
-    ))}
-  </div>
-  </Fragment>*/
+//Add GET request with path '/'
 
 app.get("/", (_, res) => {
   res.sendFile("./public/index.html", { root: __dirname }, (err) => {
@@ -64,132 +32,130 @@ app.get("/", (_, res) => {
 
 //Add GET request with path '/todos'
 app.get('/todos', (_, res) => {
- const todos = getTodos();
-  res.setHeader("Content-Type", "application/json").send(todos);
+  const todos = getTodos();
+  if (todos) {
+    res.setHeader("Content-Type", "application/json").send(todos);
+
+  }
+  else {
+    res.status(404).send([]);
+  }
+
 });
 
+// //Add GET request with path '/todos/overdue'
+app.get('/todos/overdue', (_, res) => {
+  const todos = getTodos();
+  const overdueTodos = todos.filter((todo) => Date.parse(todo.due) < new Date() && todo.completed === false);
+  res.setHeader("Content-Type", "application/json").send(overdueTodos);
+});
+
+// //Add GET request with path '/todos/completed'.should be listening and respond with the content type set to application/json 6) should return array of completed todos 7) should return empty array if there are no completed todos
+app.get('/todos/completed', (_, res) => {
+  const todos = getTodos();
+  const completedTodos = todos.filter((todo) => todo.completed === true);
+  if (completedTodos.length && completedTodos) {
+    res.setHeader("Content-Type", "application/json").send(completedTodos);
+  }
+  else {
+    res.send([]);
+  }
+});
 
 //Add Get request with path '/todos/:id'
 app.get('/todos/:id', (_, res) => {
   const todos = getTodos();
-  const id = _.params.id;
-  const todo = todos.find((todo) => todo.id === id);
+  let id = _.params.id;
+  let todo = todos.find((todo) => todo.id === id);
   if (todo) {
     res.setHeader("Content-Type", "application/json").send(todo);
   }
   else {
+    res.status(404).send([]);
+  }
+
+});
+
+// //Add POST request with path '/todos'
+app.post('/todos', (req, res) => {
+  const todos = getTodos();
+  const id = req.params.id;
+  const { name, due } = req.body;
+  if (req.body && new Date(due) != "Invalid Date") {
+    const newTodo = {
+      id: uuidv4(),
+      name,
+      created: new Date().toISOString(),
+      due,
+      completed: false,
+  };
+    todos.push(newTodo);
+    const todosJSON = JSON.stringify(todos && newTodo, null, 2);
+    fs.writeFileSync("newTodo.json", todosJSON);
+    console.log(JSON.parse(todosJSON))
+    res.setHeader("Content-Type", "application/json").status(201).send(newTodo);
+  }
+  else {
+    res.status(400).send();
+  }
+});
+
+// //Add PATCH request with path '/todos/:id'
+app.patch('/todos/:id', (req, res) => {
+  const todos = getTodos();
+  let id = req.params.id;
+  let newTodo = req.body.name;
+  const todo = todos.find((todo) => todo.id === id);
+
+  if (todo) {
+    todo.name = newTodo;
+    todo.id = uuidv4()
+    console.log(todo)
+    res.setHeader("Content-Type", "application/json").status(200).send();
+  } else {
     res.status(404).send();
   }
-
 });
 
-//Add GET request with path '/todos/overdue'.
-app.get('/todos/overdue', (_, res) => {
+// //Add POST request with path '/todos/:id/complete'
+app.post('/todos/:id/complete', (req, res) => {
   const todos = getTodos();
-  const todosOverdue = todos.filter((todo) => todo.dueDate < new Date());
-  const id = _.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  if (todosOverdue) {
-    res.header("Content-Type", "application/json");
-    //console.log(todosOverdue);
-    res.status(201).send(todosOverdue);
-  }
-  else {
-    res.status(404).send([0]);
-  }
-});
+  const id = req.body.id;
 
-//Add GET request with path '/todos/completed'.
-app.get('/todos/completed', (__, res) => {
-  const todos = getTodos();
-  const id = _.params.id;
   const todo = todos.find((todo) => todo.id === id);
-  const todosCompleted = todos.filter((todo) => todo.completed === true);
-  res.header("Content-Type", "/application/json/");
-  res.send(todosCompleted);
-  // console.log(todosCompleted);
-});
-
-//Add POST request with path '/todos'
-app.post('/todos', (_, res) => {
-  const todos = getTodos();
-  const id = _.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  res.header("Content-Type", "/application/json/");
-  const newTodo = _.body;
-  if (newTodo) {
-    newTodo.id = uuidv4();
-    //uuidv4() is a function that generates a unique id
-    newTodo.id = todos.length + 1;
-    newTodo.created = new Date();
-    newTodo.completed = false;
-    todos.push(newTodo);
-    res.status(201).send(newTodo)
-  }
-  else {
-    res.status(400).send();
-  };
-});
-
-//Add PATCH request with path '/todos/:id'
-app.patch('/todos/:id', (_, res) => {
-  const todos = getTodos();
-  const id = _.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  res.header("Content-Type", "/application/json/");
-  if (todo) {
-
-    todo.description = req.body.description;
-    res.status(200).send();
+  if (todo && todo.completed === false) {
+    res.setHeader("Content-Type", "/application/json/").send(todos);
   } else {
-    res.status(400).send();
+    res.status(404).send();
   }
 });
 
-//Add POST request with path '/todos/:id/complete'.  should contain COMPLETED todo with id '19d539a11189-bb60-u663-8sd4-01507581':
-app.post('/todos/:id/complete', (_, res) => {
-  const todos = getTodos();
-  const id = _.params.id;
-  res.header("Content-Type", "/application/json/");
-  const todo = todos.find((todo) => todo.id === id);
-  if (todo) {
-    todo.completed = true;
-    res.send(todo);
-  } else {
-    res.status(404).send("Not Found");
-  }
-});
-
-//Add POST request with path '/todos/:id/undo'
+// //Add POST request with path '/todos/:id/undo'
 app.post('/todos/:id/undo', (_, res) => {
   const todos = getTodos();
   const id = _.params.id;
-  res.header("Content-Type", "/application/json/");
-  const todo = todos.find((todo) => todo.id === id);
-  if (todo) {
-    todo.completed = false;
-    res.send(todo);
+  const todo = todos.find((todo) => todo.id === id && todo.completed === true);
+
+  if (todos) {
+    res.header("Content-Type", "/application/json/").send(todo);
   } else {
     res.status(404).send();
   }
 });
 
-//Add DELETE request with path '/todos/:id'
+// //Add DELETE request with path '/todos/:id'
 app.delete('/todos/:id', (_, res) => {
   const todos = getTodos();
   const id = _.params.id;
-  res.header("Content-Type", "/application/json/");
+
   const todo = todos.find((todo) => todo.id === id);
-  if (todo) {
-    todos.splice(todos.indexOf(todo), 1);
-    res.status(200).send(todo);
+  if (todos === -1) {
+    res.setHeader("Content-Type", "/application/json/");
+    todos.splice(todos.indexOf(todos), 3);
+    res.json(todos);
   } else {
     res.status(404).send();
   }
-});
-
-app.listen(port, function () {
-  console.log(`Node server is running... http://localhost:${port}`);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-const app = require("index.js");
+const app = require("./index");
+const port =  8081;
 
 app.listen(port, function () {
   console.log(`Node server is running... http://localhost:${port}`);
 });
+
